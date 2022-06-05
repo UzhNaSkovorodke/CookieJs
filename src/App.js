0d7f793dc92f219cf47cecd22cd50f1672c230fb
UzhNaSkovorodke <piratt.kiri@gmail.com>
2022-06-05 17:24:38 +0700

    2

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
 import React from "react";
+import Form from "./form";
+import Info from "./info";
+import WeatherInfo from "./Weather-info";
 
-function App(porp) {
-  return <p>Popo {porp.name}</p>;
+const API_KEY = "6d1fb31189f01f61b6892ff3800658cf";
+
+class App extends React.Component {
+  gettingWeather = async () => {
+    const api_url = await fetch(``);
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>Hey</h1>
+        <Info></Info>
+        <Form></Form>
+        <WeatherInfo></WeatherInfo>
+      </div>
+    );
+  }
 }
-
 export default App;
